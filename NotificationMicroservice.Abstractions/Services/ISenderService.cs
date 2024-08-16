8e6d1de2ddcd05891f99e3d33c75d42c7e190bc2
barmalej24 <barmalej@inbox.ru>
2024-08-17 00:13:55 +0700

    deleted abstraction

--- NotificationMicroservice.Abstractions/Services/ISenderService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NotificationMicroservice.Domain.Interfaces.Services
-{
-    /// <summary>
-    /// Описание методов для сервиса Отправки сообщений
-    /// </summary>
-    public interface ISenderService
-    {
-    }
-}
